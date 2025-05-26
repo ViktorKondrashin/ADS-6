702adcc84c185857897db288136f564f3f911703
ViktorKondrashin <visktor.srgeev@mail.ru>
2025-05-26 22:30:26 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,6 +1,9 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <stdexcept>
+#include <iostream>
+#include <cmath>
 
 template<typename T>
 class TPQueue {
